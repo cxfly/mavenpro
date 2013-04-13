aa18f0f542057e4006a98562f85c4d83ff2c6193
lz <lz@blueSky007>
2013-04-13 10:07:22 +0800

    aaaaaaa

--- test/src/com/test/Test.java
+++ test/src/com/test/Test.java
@@ -1,6 +1,9 @@
 package com.test;
+
 public class Test {
-public static void main(String[] args) {
-    System.out.println("Hello World");
-}
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println(Thread.currentThread().getName());
+    }
 }
